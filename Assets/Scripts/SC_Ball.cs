175c5a76ec6c42aaacd0e979cec8f4004359e20c
Markus <48928451+MarkusTve@users.noreply.github.com>
2026-01-30 10:13:34 +0100

    Input and Bounces
    Added input to aim with a pointer and for releasing the ball, work in progress on how bounces should be calculated, ended up using vector2.reflect but need to work out how bounces on either side of the squares should be calculated currently works on one side.

--- Assets/Scripts/SC_Ball.cs
+++ Assets/Scripts/SC_Ball.cs
@@ -6,7 +6,10 @@ using UnityEngine.InputSystem;
 public class SC_Ball : MonoBehaviour
 {
     [SerializeField]
-    private InputActionReference playerInputActionReference;
+    private InputActionAsset inputAsset;
+
+    private InputAction pointerClickInputAction;
+    private InputAction pointerMoveInputAction;
 
     private int hitAmount = 1;
 
@@ -18,10 +21,29 @@ public class SC_Ball : MonoBehaviour
 
     Rigidbody2D rigidBody = null;
 
+    Vector2 currentDirection = Vector2.zero;
+    Vector2 mousePosition = Vector2.zero;
+
+
     private void Awake()
     {
-        playerInputActionReference.action.performed += OnActionPerformed;
+        pointerMoveInputAction = inputAsset.FindAction("Point");
+        pointerClickInputAction = inputAsset.FindAction("Click");
+
+        pointerClickInputAction.performed += OnPointerActionPerformed;
+
         rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnPointerActionPerformed(InputAction.CallbackContext context)
+    {
+        if (shouldMove)
+            return;
+
+        currentDirection = mousePosition - (Vector2)transform.position;
+        currentDirection.Normalize();
+
+        shouldMove = true;
     }
 
     private void FixedUpdate()
@@ -29,15 +51,16 @@ public class SC_Ball : MonoBehaviour
         if (!shouldMove)
             return;
 
-        rigidBody.linearVelocity = speed * Time.fixedDeltaTime * Vector2.up;
+        rigidBody.linearVelocity = speed * Time.fixedDeltaTime * currentDirection;
     }
-    private void OnActionPerformed(InputAction.CallbackContext obj)
-    {
-        if (shouldMove)
-            return;
 
-        shouldMove = true;
+    private void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(pointerMoveInputAction.ReadValue<Vector2>());
+
+        Debug.DrawLine(this.transform.position, mousePosition);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.CompareTag("Box"))
@@ -48,6 +71,18 @@ public class SC_Ball : MonoBehaviour
 
     private void OnBounce(Collider2D objectToBounceOff)
     {
-        Vector2 currentVelocity = rigidBody.linearVelocity;
+        Vector2 newDirection;
+
+        Vector2 surfaceNormal = Vector2.zero;
+
+        surfaceNormal = objectToBounceOff.ClosestPoint(this.transform.position) - (Vector2)objectToBounceOff.bounds.center;
+
+        surfaceNormal.Normalize();
+
+        // Change so that Vector2.down is a vector that chages depending on what side of the square was hit
+        newDirection = Vector2.Reflect(rigidBody.linearVelocity.normalized, Vector2.down);
+
+        currentDirection = newDirection;
+
     }
 }
